f58494cd669fbea7fb0b058386fdfa4284b6f0b1
uday-bhatiya <udaybhatiya31@gmail.com>
2025-11-12 11:02:40 +0530

    refactor routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router,  Routes, Route, Navigate} from "react-router-dom"
+import { BrowserRouter as Router,  Routes, Route} from "react-router-dom"
 import Home from "./pages/Home"
 import Login from "./pages/Login"
 import Signup from "./pages/Signup"
@@ -8,11 +8,9 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Navigate to="/login" replace />} />
-
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
-        <Route path="/home" element={<Home />} />
+        <Route path="/" element={<Home />} />
       </Routes>
     </Router>
   )
