aa1fa7028bacbd5e39a95c8876346699a65c6239
uday-bhatiya <udaybhatiya31@gmail.com>
2025-11-12 10:47:48 +0530

    add navigation redirect from root to login page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router,  Routes, Route} from "react-router-dom"
+import { BrowserRouter as Router,  Routes, Route, Navigate} from "react-router-dom"
 import Home from "./pages/Home"
 import Login from "./pages/Login"
 import Signup from "./pages/Signup"
@@ -8,9 +8,11 @@ function App() {
   return (
     <Router>
       <Routes>
+        <Route path="/" element={<Navigate to="/login" replace />} />
+
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
-        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
       </Routes>
     </Router>
   )
